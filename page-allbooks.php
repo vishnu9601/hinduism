9e0c0ebd6aea4badee6896785ec09847aca8fea1
rites2 <architanand320@gmail.com>
2023-08-18 11:33:20 +0530

    hi

--- page-allbooks.php
+++ page-allbooks.php
@@ -1,6 +1,35 @@
 <?php 
  get_header();
 
+ $args = array(
+
+    // Arguments for your query.
+
+);
+
+// Custom query.
+
+$query = new WP_Query( $args );
+
+// Check that we have query results.
+
+if ( $query->have_posts() ) {
+
+    // Start looping over the query results.
+
+    while ( $query->have_posts() ) {
+
+        $query->the_post();
+
+        // Contents of the queried post results go here.
+
+    }
+
+}
+
+// Restore original post data.
+
+wp_reset_postdata();
 
 
  get_footer();
