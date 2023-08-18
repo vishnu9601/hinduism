56f0c9ca9238ae5207872ff1de94be8ce28187e7
rites2 <architanand320@gmail.com>
2023-08-18 22:10:06 +0530

    c ommit

--- page-allbooks.php
+++ page-allbooks.php
@@ -11,19 +11,27 @@ $query = new WP_Query( $args );
 
 if ( $query->have_posts() ) {
     echo '<table>';
-    echo '<tr><th>Title</th><th>PDF Link</th></tr>';
+    echo '<tr><th>Title</th><th>PDF Link</th><th>Description</th></tr>';
 
     while ( $query->have_posts() ) {
         $query->the_post();
         $pdf_url = wp_get_attachment_url();
         $title = get_the_title();
-
+       
         echo '<tr>';
         echo '<td>' . esc_html( $title ) . '</td>';
         echo '<td><a href="' . esc_url( $pdf_url ) . '">Download PDF</a></td>';
         echo '</tr>';
+   
+        echo '<tr>';
+        $query->the_content();
+        
+        $description = get_the_content();
+        echo '</tr>';
+        
     }
 
+    
     echo '</table>';
 
     wp_reset_postdata(); // Reset the post data after the loop
@@ -33,6 +41,5 @@ if ( $query->have_posts() ) {
 // Restore original post data.
 wp_reset_postdata();
 
-
- get_footer();
+get_footer();
 ?>
