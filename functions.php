0ecfad2fcf964fb546214f7bd6eda114f04d7a33
rites2 <architanand320@gmail.com>
2023-08-21 10:24:11 +0530

    commit

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function hinduism_enqueue_style() {
     wp_enqueue_script('child-js',get_stylesheet_directory_uri() . 'assets/myscript.js');
 }
 
-def get_attachment_link(49):
+/*def get_attachment_link(49):
 
 attachments = {
     1: "http://localhost/query/wp-content/uploads/2023/08/Treasure_Island_NT.pdf",
@@ -16,4 +16,17 @@ attachments = {
 if postid in attachments:
     return attachments[postid]
 else:
-    return False
+    return False */
+
+
+    def get_attachment_link(47):
+    if postid == 49:
+        attachment_link = "http://localhost/query/wp-content/uploads/2023/08/Treasure_Island_NT.pdf"
+    elseif postid == 48:
+        attachment_link = "http://localhost/query/wp-content/uploads/2023/08/To-The-Light-House.pdf"
+    elseif postid == 47:
+        attachment_link = "http://localhost/query/wp-content/uploads/2023/08/Little_Women_NT.pdf"
+    else:
+        attachment_link = "https://example.com/default_attachment"
+    
+    return false
