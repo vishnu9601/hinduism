6b977719893c8e45d3d312831b58ef60b2439aca
Adarsh Akshat <adarshakshat21@outlook.com>
2023-08-22 13:31:49 +0530

    Updated UI with ACF

--- attachment.php
+++ attachment.php
@@ -10,9 +10,13 @@ if($current_attachment_type['ext']==='pdf'){
     $writer= get_field('writer');
     $publication= get_field('publication');
 
+    $book_details = '<p class="book-info"><strong>Language:</strong>'. $language . '</p>
+    <p class="book-info"><strong>Publication:</strong>'.$publication.'</p>
+    <p class="book-info"><strong>Writer:</strong> '.$writer.'</p>';
     $download_button = '<a href="'. $download_link .'"><button class="">Download</button></a>';
     $content = '<div>' . get_the_content() . '</div>';
-    $vishnu = '<div class="wrap" style="height: 100vh;display: flex;flex-direction: column;align-items: center;width: 100%;justify-content: center;"><h1>' . $book_name . '</h1>'. $content . $download_button . '</div>';
+    $vishnu = '<div class="wrap" style="height: 100vh;display: flex;flex-direction: column;align-items: center;width: 100%;justify-content: center;"><h1>' . $book_name . '</h1>'. $book_details .$content . $download_button . '</div>';
+    
     echo $vishnu;
     echo $language;
     echo $writer;
--- functions.php
+++ functions.php
@@ -4,8 +4,4 @@ function hinduism_enqueue_style() {
     wp_enqueue_style('child-style', get_stylesheet_directory_uri() . 'assets/mystyle.css');
     wp_enqueue_script('child-js',get_stylesheet_directory_uri() . 'assets/myscript.js');
 }
-    $post->ID='583';
-    $var_name= get_field('book_name', 583);
-    /* field name means which u gave custom field name*/
-    echo $var_name;
-    
+    include_once get_stylesheet_directory() . '/acf-config.php';
