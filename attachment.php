b02f6a66bd12b97729b3e03b39368a1bf19fc880
rites2 <architanand320@gmail.com>
2023-08-22 13:42:59 +0530

    commit

--- attachment.php
+++ attachment.php
@@ -4,15 +4,9 @@ $download_link = wp_get_attachment_url();
 $current_attachment_type = wp_check_filetype($download_link);
 if($current_attachment_type['ext']==='pdf'){
     get_header();
-
-    $book_name= get_field('book_name');
-    $language= get_field('language');
-    $writer= get_field('writer');
-    $publication= get_field('publication');
-
     $download_button = '<a href="'. $download_link .'"><button class="">Download</button></a>';
     $content = '<div>' . get_the_content() . '</div>';
-    $vishnu = '<div class="wrap" style="height: 100vh;display: flex;flex-direction: column;align-items: center;width: 100%;justify-content: center;"><h1>' . $book_name . '</h1>'. $content . $download_button . '</div>';
+    $vishnu = '<div class="wrap" style="height: 100vh;display: flex;flex-direction: column;align-items: center;width: 100%;justify-content: center;"><h1>' . get_the_title() . '</h1>'. $content . $download_button . '</div>';
     echo $vishnu;
     comments_template();
     get_footer();
