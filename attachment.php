2d26177c92467fdb061d7d0293815d1b60c45fc7
rites2 <architanand320@gmail.com>
2023-08-22 13:14:42 +0530

    commited

--- attachment.php
+++ attachment.php
@@ -4,10 +4,21 @@ $download_link = wp_get_attachment_url();
 $current_attachment_type = wp_check_filetype($download_link);
 if($current_attachment_type['ext']==='pdf'){
     get_header();
+
+    $book_name= get_field('book_name');
+    $language= get_field('language');
+    $writer= get_field('writer');
+    $publication= get_field('publication');
+
+
+  
     $download_button = '<a href="'. $download_link .'"><button class="">Download</button></a>';
     $content = '<div>' . get_the_content() . '</div>';
-    $vishnu = '<div class="wrap" style="height: 100vh;display: flex;flex-direction: column;align-items: center;width: 100%;justify-content: center;"><h1>' . get_the_title() . '</h1>'. $content . $download_button . '</div>';
+    $vishnu = '<div class="wrap" style="height: 100vh;display: flex;flex-direction: column;align-items: center;width: 100%;justify-content: center;"><h1>' . $book_name . '</h1>'. $content . $download_button . '</div>';
     echo $vishnu;
+    echo $language;
+    echo $writer;
+    echo $publication;
     comments_template();
     get_footer();
 
@@ -20,10 +31,6 @@ else {
 }
 
 
-    $post->ID='583';
-    $var_name= get_field('book_name', 583);
-    /* field name means which u gave custom field name*/
-    echo $var_name;
 
 
 
